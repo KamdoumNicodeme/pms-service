cf853c3b3a0a91f462f4db151a75e1a752574991
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-06-11 13:01:35 +0200

    Update OnboardingTaskListBuilderServiceTest.java

--- OnboardingTaskListBuilderServiceTest.java
+++ OnboardingTaskListBuilderServiceTest.java
@@ -1,79 +1,103 @@
-ExtendWith(MockitoExtension.class)
+@ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
-class ThirdPartyAdditionFieldsBuilderServiceTest {
+class OnboardingThirdPartyFieldsBuilderServiceTest extends OnboardingFieldsBuilderTestSupport {
 
     @InjectMocks
-    private ThirdPartyAdditionFieldsBuilderService thirdPartyAdditionFieldsBuilderService;
-
-    @Mock
-    private ReferenceDataRepositoryService referenceDataRepositoryService;
-
-    @BeforeEach
-    void initMock() {
-
-        Mockito.when(referenceDataRepositoryService.getReferenceDataOptionsByDomain(THIRD_PARTY_TYPE_DOMAIN))
-                .thenReturn(ReferenceServiceHelper.getThirdPartyTypeOptions());
-        Mockito.when(referenceDataRepositoryService.getReferenceDataOptionsByDomainAndSelectedValue(COUNTRY_DOMAIN, "BE"))
-                .thenReturn(List.of(new SelectInputFieldOption("BE", "Belgium")));
-        Mockito.when(referenceDataRepositoryService.getReferenceDataOptionsByDomainAndSelectedValue(COUNTRY_DOMAIN, "N/A"))
-                .thenReturn(List.of(new SelectInputFieldOption("N/A", "N/A")));
-    }
+    private OnboardingThirdPartyFieldsBuilderService builderService;
 
     @Test
-    void builderAddedInfoFields_OK() {
+    void buildThirdPartyFieldsOK() {
+        Policy policy = policy();
+        Group thirdParty = group(THIRD_PARTY_GROUP);
+        WebForm webForm = webForm();
+        BusinessTransaction transaction = transaction();
 
-        Policy policy = PolicyBuilderServiceHelper.createPolicy();
-        Group addedInfo = Group.builder().groupId(RulesConstants.THIRD_PARTY_GROUP).build();
-        WebForm webForm = WebFormBuilderServiceHelper.createWebForm();
-        BusinessTransaction transaction = TransactionBuilderServiceHelper.createTransaction();
-        /* Used to automatically increment "order" by 1 for each individual field test. Makes adding/removing a field less painful */
         AtomicInteger testOrder = new AtomicInteger(1);
-        int expectedNumberOfFields = 13;
+        int expectedNumberOfFields = 10;
 
-        thirdPartyAdditionFieldsBuilderService.buildField(webForm, new ScreenDescription(), addedInfo, policy, transaction,
-                Map.of("BLOCKED", new ArrayList<>()));
-        assertEquals(expectedNumberOfFields, addedInfo.getFields().size());
+        builderService.buildField(
+                webForm,
+                screenDescription(),
+                thirdParty,
+                policy,
+                transaction,
+                overallCaseRisk()
+        );
 
-        FieldHelper.testFieldValueAndIncr(THIRD_PARTY_NAME + "_1", TextInputField.class, addedInfo, "909090", testOrder, null, true, false);
-        FieldHelper.testSelectFieldValueAndIncr(LINK_THIRD_PARTY_PH + "_1", SelectInputField.class, addedInfo, "originator", testOrder, null, true,
-                false, 0);
-        FieldHelper.testFieldValueAndIncr(REASON_THIRD_PARTY_PAYMENT + "_1", TextInputField.class, addedInfo, null, testOrder, null, false, true);
-        FieldHelper.testSelectFieldValueAndIncr(THIRD_PARTY_COUNTRY + "_1", SelectInputField.class, addedInfo, null, testOrder, null, true, false,
-                0);
-        FieldHelper.testFieldValueAndIncr(THIRD_PARTY_NAME + "_2", TextInputField.class, addedInfo, "123456789", testOrder, null, true, false);
-        FieldHelper.testSelectFieldValueAndIncr(LINK_THIRD_PARTY_PH + "_2", SelectInputField.class, addedInfo, "originator", testOrder, null, true,
-                false, 0);
-        FieldHelper.testFieldValueAndIncr(REASON_THIRD_PARTY_PAYMENT + "_2", TextInputField.class, addedInfo, null, testOrder, null, false, true);
-        FieldHelper.testSelectFieldValueAndIncr(THIRD_PARTY_COUNTRY + "_2", SelectInputField.class, addedInfo, null, testOrder, null, true, false,
-                0);
-        FieldHelper.testFieldValueAndIncr(THIRD_PARTY_NAME + "_3", TextInputField.class, addedInfo, "99118822", testOrder, null, true, false);
-        FieldHelper.testSelectFieldValueAndIncr(LINK_THIRD_PARTY_PH + "_3", SelectInputField.class, addedInfo, "originator", testOrder, null, true,
-                false, 0);
-        FieldHelper.testFieldValueAndIncr(REASON_THIRD_PARTY_PAYMENT + "_3", TextInputField.class, addedInfo, null, testOrder, null, false, true);
-        FieldHelper.testSelectFieldValueAndIncr(THIRD_PARTY_COUNTRY + "_3", SelectInputField.class, addedInfo, null, testOrder, null, true, false,
-                0);
-        FieldHelper.testFieldValueAndIncr(THIRD_PARTY_COUNTRY_RISK, TextInputField.class, addedInfo, "STANDARD", testOrder, null, false, false);
+        assertEquals(expectedNumberOfFields, thirdParty.getFields().size());
 
-        /* To make sure we accounted for all fields built in the group */
+        FieldHelper.testFieldValueAndIncr(THIRD_PARTY_NAME + "_1", TextInputField.class,
+                thirdParty, "909090", testOrder, null, true, null);
+
+        FieldHelper.testSelectFieldValueAndIncr(LINK_THIRD_PARTY_PH + "_1", SelectInputField.class,
+                thirdParty, "originator", testOrder, null, true, null, 1);
+
+        FieldHelper.testFieldValueAndIncr(REASON_THIRD_PARTY_PAYMENT + "_1", TextInputField.class,
+                thirdParty, null, testOrder, null, null, true);
+
+        FieldHelper.testSelectFieldValueAndIncr(THIRD_PARTY_COUNTRY + "_1", SelectInputField.class,
+                thirdParty, null, testOrder, null, true, null, 0);
+
+        FieldHelper.testFieldValueAndIncr(THIRD_PARTY_NAME + "_2", TextInputField.class,
+                thirdParty, "123456789", testOrder, null, true, null);
+
+        FieldHelper.testSelectFieldValueAndIncr(LINK_THIRD_PARTY_PH + "_2", SelectInputField.class,
+                thirdParty, "originator", testOrder, null, true, null, 1);
+
+        FieldHelper.testFieldValueAndIncr(REASON_THIRD_PARTY_PAYMENT + "_2", TextInputField.class,
+                thirdParty, null, testOrder, null, null, true);
+
+        FieldHelper.testSelectFieldValueAndIncr(THIRD_PARTY_COUNTRY + "_2", SelectInputField.class,
+                thirdParty, null, testOrder, null, true, null, 0);
+
+        FieldHelper.testFieldValueAndIncr(THIRD_PARTY_COUNTRY_RISK, TextInputField.class,
+                thirdParty, "STANDARD", testOrder, null, true, null);
+
+        FieldHelper.testFieldValueAndIncr(ADDITIONAL_INFO_LINK, TextAreaField.class,
+                thirdParty, null, testOrder, "#LINK_THIRD_PARTY_PH# == \"other\"", false, true);
+
         assertEquals(expectedNumberOfFields, testOrder.get() - 1);
+
+        verify(referenceDataRepositoryService, atLeastOnce())
+                .getReferenceDataOptionsByDomainAndSelectedValue(eq(THIRD_PARTY_TYPE_DOMAIN), any());
+
+        verify(referenceDataRepositoryService, atLeastOnce())
+                .getReferenceDataOptionsByDomainAndSelectedValue(eq(COUNTRY_DOMAIN), any());
     }
 
     @Test
-    void builderBankCountry_NA() {
+    void buildThirdPartyFieldsWithoutPaymentDetailsOK() {
+        Policy policy = policy();
+        Group thirdParty = group(THIRD_PARTY_GROUP);
+        BusinessTransaction transaction = transaction();
 
-        Policy policy = PolicyBuilderServiceHelper.createPolicy();
-        Group generalDetails = Group.builder().groupId(RulesConstants.THIRD_PARTY_GROUP).build();
-        WebForm webForm = WebForm.builder()
-                .groups(Collections.singletonList(
-                        WebFormGroup.builder().textFields(List.of(TextWebFormField.builder().fieldId("BANK_COUNTRY").value("").build())).build()))
-                .build();
-        BusinessTransaction transaction = TransactionBuilderServiceHelper.createTransaction();
-        thirdPartyAdditionFieldsBuilderService.buildField(webForm, new ScreenDescription(), generalDetails, policy, transaction,
-                Map.of("BLOCKED", new ArrayList<>()));
-        assertEquals(13, generalDetails.getFields().size());
+        transaction.setPaymentDetails(Collections.emptyList());
 
-        FieldHelper.testSelectFieldValue(THIRD_PARTY_COUNTRY + "_1", SelectInputField.class, generalDetails, null, 4, null, true, false, 0);
-        FieldHelper.testSelectFieldValue(THIRD_PARTY_COUNTRY + "_2", SelectInputField.class, generalDetails, null, 8, null, true, false, 0);
-        FieldHelper.testFieldValue(THIRD_PARTY_COUNTRY_RISK, TextInputField.class, generalDetails, "STANDARD", 13, null, false, false);
+        AtomicInteger testOrder = new AtomicInteger(1);
+        int expectedNumberOfFields = 2;
+
+        builderService.buildField(
+                webForm(),
+                screenDescription(),
+                thirdParty,
+                policy,
+                transaction,
+                overallCaseRisk()
+        );
+
+        assertEquals(expectedNumberOfFields, thirdParty.getFields().size());
+
+        FieldHelper.testMissingField(THIRD_PARTY_NAME + "_1", TextInputField.class, thirdParty);
+        FieldHelper.testMissingField(LINK_THIRD_PARTY_PH + "_1", SelectInputField.class, thirdParty);
+        FieldHelper.testMissingField(REASON_THIRD_PARTY_PAYMENT + "_1", TextInputField.class, thirdParty);
+        FieldHelper.testMissingField(THIRD_PARTY_COUNTRY + "_1", SelectInputField.class, thirdParty);
+
+        FieldHelper.testFieldValueAndIncr(THIRD_PARTY_COUNTRY_RISK, TextInputField.class,
+                thirdParty, "STANDARD", testOrder, null, true, null);
+
+        FieldHelper.testFieldValueAndIncr(ADDITIONAL_INFO_LINK, TextAreaField.class,
+                thirdParty, null, testOrder, "#LINK_THIRD_PARTY_PH# == \"other\"", false, true);
+
+        assertEquals(expectedNumberOfFields, testOrder.get() - 1);
     }
 }
