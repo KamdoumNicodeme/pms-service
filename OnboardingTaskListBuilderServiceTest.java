e51b681abe7c71e5d818802235ddedf4387d36b5
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-06-11 12:53:26 +0200

    Update OnboardingTaskListBuilderServiceTest.java

--- OnboardingTaskListBuilderServiceTest.java
+++ OnboardingTaskListBuilderServiceTest.java
@@ -1,134 +1,236 @@
-@Service
-@RequiredArgsConstructor
-public class OnboardingThirdPartyFieldsBuilderService extends OnboardingChecklistFieldsBuilderSupport implements FieldBuilderService {
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+class OnboardingThirdPartyFieldsBuilderServiceTest extends OnboardingFieldsBuilderTestSupport {
 
-    private final ReferenceDataRepositoryService referenceDataRepositoryService;
+    @InjectMocks
+    private OnboardingThirdPartyFieldsBuilderService builderService;
 
-    @Override
-    public void buildField(final WebForm webForm, final ScreenDescription screenDescription, final Group group, final Policy policy,
-            final BusinessTransaction transaction, final Map<String,List<String>> overallCaseRisk) {
+    @Test
+    void buildThirdPartyFieldsOK() {
+        var group = group(THIRD_PARTY_GROUP);
+        var transaction = transaction();
+        var order = new AtomicInteger(1);
 
-        Field.resetFieldId();
+        builderService.buildField(
+                webForm(),
+                screenDescription(),
+                group,
+                policy(),
+                transaction,
+                overallCaseRisk()
+        );
 
-        var payments = ChecklistUtils.getMoneyInTransactionPaymentDetails(transaction);
-        IntStream.range(1, payments.size() + 1).forEach(i -> {
-            var paymentDetails = payments.get(i - 1);
-            evaluateThirdPartyName(group, paymentDetails, i);
-            evaluateLinkThirdPartyPh(group, paymentDetails, i, referenceDataRepositoryService);
-            evaluateReasonThirdPartyPayment(group, i);
-            evaluateThirdPartyCountry(group, paymentDetails, i, referenceDataRepositoryService);
-        });
+        assertEquals(10, group.getFields().size());
 
-        evaluateThirdPartyCountryRisk(group, transaction);
-        evaluateAdditionalInfoLink(group);
+        FieldHelper.testFieldValueAndIncr(
+                THIRD_PARTY_NAME + "_1",
+                TextInputField.class,
+                group,
+                null,
+                order,
+                null,
+                true,
+                null
+        );
 
+        FieldHelper.testSelectFieldValue(
+                LINK_THIRD_PARTY_PH + "_1",
+                SelectInputField.class,
+                group,
+                null,
+                order.getAndIncrement(),
+                null,
+                true,
+                null,
+                0
+        );
+
+        FieldHelper.testFieldValueAndIncr(
+                REASON_THIRD_PARTY_PAYMENT + "_1",
+                TextInputField.class,
+                group,
+                null,
+                order,
+                null,
+                null,
+                true
+        );
+
+        FieldHelper.testSelectFieldValue(
+                THIRD_PARTY_COUNTRY + "_1",
+                SelectInputField.class,
+                group,
+                null,
+                order.getAndIncrement(),
+                null,
+                true,
+                null,
+                0
+        );
+
+        FieldHelper.testFieldValueAndIncr(
+                THIRD_PARTY_NAME + "_2",
+                TextInputField.class,
+                group,
+                null,
+                order,
+                null,
+                true,
+                null
+        );
+
+        FieldHelper.testSelectFieldValue(
+                LINK_THIRD_PARTY_PH + "_2",
+                SelectInputField.class,
+                group,
+                null,
+                order.getAndIncrement(),
+                null,
+                true,
+                null,
+                0
+        );
+
+        FieldHelper.testFieldValueAndIncr(
+                REASON_THIRD_PARTY_PAYMENT + "_2",
+                TextInputField.class,
+                group,
+                null,
+                order,
+                null,
+                null,
+                true
+        );
+
+        FieldHelper.testSelectFieldValue(
+                THIRD_PARTY_COUNTRY + "_2",
+                SelectInputField.class,
+                group,
+                null,
+                order.getAndIncrement(),
+                null,
+                true,
+                null,
+                0
+        );
+
+        FieldHelper.testFieldValueAndIncr(
+                THIRD_PARTY_COUNTRY_RISK,
+                TextInputField.class,
+                group,
+                RulesUtils.getRiskFactorLevel(transaction, INT_RF_016),
+                order,
+                null,
+                null,
+                null
+        );
+
+        FieldHelper.testFieldValueAndIncr(
+                ADDITIONAL_INFO_LINK,
+                TextAreaField.class,
+                group,
+                null,
+                order,
+                "#LINK_THIRD_PARTY_PH# == \"other\"",
+                false,
+                true
+        );
+
+        assertEquals(10, order.get() - 1);
     }
 
-    private void evaluateAdditionalInfoLink(Group group) {
+    @Test
+    void buildThirdPartyFieldsShouldPopulatePaymentDetailsValues() {
+        var group = group(THIRD_PARTY_GROUP);
+        var transaction = transaction();
 
-        var additionalInfoLink = (TextAreaField) ChecklistUtils.getFieldInGroup(group, ADDITIONAL_INFO_LINK);
-        if (additionalInfoLink == null) {
-            additionalInfoLink = TextAreaField.builder().fieldId(ADDITIONAL_INFO_LINK).build();
-            group.getFields().add(additionalInfoLink);
+        builderService.buildField(
+                webForm(),
+                screenDescription(),
+                group,
+                policy(),
+                transaction,
+                overallCaseRisk()
+        );
+
+        var firstPayment = ChecklistUtils.getMoneyInTransactionPaymentDetails(transaction).getFirst();
+
+        FieldHelper.testFieldValue(
+                THIRD_PARTY_NAME + "_1",
+                TextInputField.class,
+                group,
+                firstPayment.getPayerID(),
+                1,
+                null,
+                true,
+                null
+        );
+
+        var link = (SelectInputField) ChecklistUtils.getFieldInGroup(group, LINK_THIRD_PARTY_PH + "_1");
+        var country = (SelectInputField) ChecklistUtils.getFieldInGroup(group, THIRD_PARTY_COUNTRY + "_1");
+
+        assertNotNull(link);
+        assertNotNull(country);
+
+        if (firstPayment.getOriginator() != null) {
+            assertEquals(firstPayment.getOriginator().getExternalId(), link.getSelectedValue());
+            assertEquals(1, link.getOptions().size());
         }
-        additionalInfoLink.setIsActive(true);
-        additionalInfoLink.incrementOrder();
-        additionalInfoLink.setLabel("Additional info on link between 3rd party and PH");
-        additionalInfoLink.setEnabled(true);
-        additionalInfoLink.setMandatory(false);
-        additionalInfoLink.setDisplayIf("#LINK_THIRD_PARTY_PH# == \"other\"");
-        additionalInfoLink.setLabelBold(false);
-        additionalInfoLink.setSourceSystem(null);
+
+        if (firstPayment.getPayerLegalAddressCountry() != null) {
+            assertEquals(firstPayment.getPayerLegalAddressCountry().getIsoCountryCode(), country.getSelectedValue());
+            assertEquals(1, country.getOptions().size());
+        }
+
+        verify(referenceDataRepositoryService, atLeastOnce())
+                .getReferenceDataOptionsByDomainAndSelectedValue(eq(THIRD_PARTY_TYPE_DOMAIN), any());
+
+        verify(referenceDataRepositoryService, atLeastOnce())
+                .getReferenceDataOptionsByDomainAndSelectedValue(eq(COUNTRY_DOMAIN), any());
     }
 
+    @Test
+    void buildThirdPartyFieldsWithoutPaymentDetailsShouldOnlyBuildRiskAndAdditionalInfo() {
+        var group = group(THIRD_PARTY_GROUP);
+        var transaction = transaction();
+        transaction.setPaymentDetails(List.of());
+
+        builderService.buildField(
+                webForm(),
+                screenDescription(),
+                group,
+                policy(),
+                transaction,
+                overallCaseRisk()
+        );
+
+        assertEquals(2, group.getFields().size());
+
+        FieldHelper.testMissingField(THIRD_PARTY_NAME + "_1", TextInputField.class, group);
+        FieldHelper.testMissingField(LINK_THIRD_PARTY_PH + "_1", SelectInputField.class, group);
+        FieldHelper.testMissingField(REASON_THIRD_PARTY_PAYMENT + "_1", TextInputField.class, group);
+        FieldHelper.testMissingField(THIRD_PARTY_COUNTRY + "_1", SelectInputField.class, group);
+
+        FieldHelper.testFieldValue(
+                THIRD_PARTY_COUNTRY_RISK,
+                TextInputField.class,
+                group,
+                RulesUtils.getRiskFactorLevel(transaction, INT_RF_016),
+                1,
+                null,
+                null,
+                null
+        );
+
+        FieldHelper.testFieldValue(
+                ADDITIONAL_INFO_LINK,
+                TextAreaField.class,
+                group,
+                null,
+                2,
+                "#LINK_THIRD_PARTY_PH# == \"other\"",
+                false,
+                true
+        );
+    }
 }
-
-public class CommonThirdPartyFieldsBuilderService {
-
-    public static final String SOURCE_SYSTEM = "From Class";
-
-    public static void evaluateThirdPartyName(final Group group, final PaymentDetails paymentDetails, int position) {
-
-        var thirdPartyName = (TextInputField) ChecklistUtils.getFieldInGroup(group, THIRD_PARTY_NAME + "_" + position);
-        if (thirdPartyName == null) {
-            thirdPartyName = TextInputField.builder().fieldId(THIRD_PARTY_NAME + "_" + position).build();
-            group.getFields().add(thirdPartyName);
-        }
-        thirdPartyName.setIsActive(true);
-        thirdPartyName.incrementOrder();
-        thirdPartyName.setLabel("3rd party ID #" + position);
-        thirdPartyName.setSourceSystem(SOURCE_SYSTEM);
-        thirdPartyName.setMandatory(true);
-        thirdPartyName.setSelectedValue(paymentDetails != null ? paymentDetails.getPayerID() : null);
-    }
-
-    public static void evaluateLinkThirdPartyPh(final Group group, PaymentDetails paymentDetails, int position,
-            final ReferenceDataRepositoryService referenceDataRepositoryService) {
-
-        var linkThirdPartyPh = (SelectInputField) ChecklistUtils.getFieldInGroup(group, LINK_THIRD_PARTY_PH + "_" + position);
-        if (linkThirdPartyPh == null) {
-            linkThirdPartyPh = SelectInputField.builder().fieldId(LINK_THIRD_PARTY_PH + "_" + position).build();
-            group.getFields().add(linkThirdPartyPh);
-        }
-        linkThirdPartyPh.setIsActive(true);
-        linkThirdPartyPh.incrementOrder();
-        linkThirdPartyPh.setLabel("Link between 3rd party and PH #" + position);
-        linkThirdPartyPh.setMandatory(true);
-        linkThirdPartyPh.setSourceSystem(SOURCE_SYSTEM);
-        if (paymentDetails != null && paymentDetails.getOriginator() != null) {
-            linkThirdPartyPh.setSelectedValue(paymentDetails.getOriginator().getExternalId());
-        } else {
-            linkThirdPartyPh.setSelectedValue(null);
-        }
-        linkThirdPartyPh.setOptions(referenceDataRepositoryService.getReferenceDataOptionsByDomainAndSelectedValue(THIRD_PARTY_TYPE_DOMAIN,
-                linkThirdPartyPh.getSelectedValue()));
-    }
-
-    public static void evaluateReasonThirdPartyPayment(final Group group, int position) {
-
-        var reasonThirdPartyPayment = (TextInputField) ChecklistUtils.getFieldInGroup(group, REASON_THIRD_PARTY_PAYMENT + "_" + position);
-        if (reasonThirdPartyPayment == null) {
-            reasonThirdPartyPayment = TextInputField.builder().fieldId(REASON_THIRD_PARTY_PAYMENT + "_" + position).build();
-            group.getFields().add(reasonThirdPartyPayment);
-        }
-        reasonThirdPartyPayment.setIsActive(true);
-        reasonThirdPartyPayment.incrementOrder();
-        reasonThirdPartyPayment.setLabel("Reason for 3rd party payment #" + position);
-        reasonThirdPartyPayment.setEnabled(true);
-    }
-
-    public static void evaluateThirdPartyCountry(final Group group, PaymentDetails paymentDetails, int position,
-            final ReferenceDataRepositoryService referenceDataRepositoryService) {
-
-        var thirdPartyCountry = (SelectInputField) ChecklistUtils.getFieldInGroup(group, THIRD_PARTY_COUNTRY + "_" + position);
-        if (thirdPartyCountry == null) {
-            thirdPartyCountry = SelectInputField.builder().fieldId(THIRD_PARTY_COUNTRY + "_" + position).build();
-            group.getFields().add(thirdPartyCountry);
-        }
-        thirdPartyCountry.setIsActive(true);
-        thirdPartyCountry.incrementOrder();
-        thirdPartyCountry.setLabel("3rd party country #" + position);
-        thirdPartyCountry.setMandatory(true);
-        thirdPartyCountry.setSourceSystem(SOURCE_SYSTEM);
-        if (paymentDetails != null && paymentDetails.getPayerLegalAddressCountry() != null) {
-            thirdPartyCountry.setSelectedValue(paymentDetails.getPayerLegalAddressCountry().getIsoCountryCode());
-        } else {
-            thirdPartyCountry.setSelectedValue(null);
-        }
-        thirdPartyCountry.setOptions(referenceDataRepositoryService.getReferenceDataOptionsByDomainAndSelectedValue(COUNTRY_DOMAIN,
-                thirdPartyCountry.getSelectedValue()));
-    }
-
-    public static void evaluateThirdPartyCountryRisk(final Group group, final BusinessTransaction transaction) {
-
-        var thirdPartyCountryRisk = (TextInputField) ChecklistUtils.getFieldInGroup(group, THIRD_PARTY_COUNTRY_RISK);
-        if (thirdPartyCountryRisk == null) {
-            thirdPartyCountryRisk = TextInputField.builder().fieldId(THIRD_PARTY_COUNTRY_RISK).build();
-            group.getFields().add(thirdPartyCountryRisk);
-        }
-        thirdPartyCountryRisk.setIsActive(true);
-        thirdPartyCountryRisk.incrementOrder();
-        thirdPartyCountryRisk.setLabel("Highest 3rd party risk");
-        thirdPartyCountryRisk.setSourceSystem("Highest 3rd party risk from Class");
-        thirdPartyCountryRisk.setSelectedValue(RulesUtils.getRiskFactorLevel(transaction, INT_RF_016));
-    }
