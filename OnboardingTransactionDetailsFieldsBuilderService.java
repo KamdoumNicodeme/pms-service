e211b379dd3987d422e6fdab0997ff1867a36a55
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-06-21 17:39:27 +0200

    Update OnboardingTransactionDetailsFieldsBuilderService.java

--- OnboardingTransactionDetailsFieldsBuilderService.java
+++ OnboardingTransactionDetailsFieldsBuilderService.java
@@ -1,8 +1,6 @@
-private String resolveCustodianCountry(final WebFormGroup fund) {
-    return Optional.ofNullable(
-                    getGroupInGroup(fund, CUSTODIAN_BANK_DESIGNATION))
-            .map(custodian -> getGroupInGroup(custodian, ADDRESS))
-            .map(address -> getFieldInGroup(address, COUNTRY))
+private String resolveCustodianBankName(final WebFormGroup fund) {
+    return Optional.ofNullable(getGroupInGroup(fund, CUSTODIAN_BANK_DESIGNATION))
+            .map(custodian -> getFieldInGroup(custodian, COMPANY_NAME))
             .map(WebformUtils::getValueForWebFormField)
             .filter(StringUtils::isNotBlank)
             .orElse("N/A");
