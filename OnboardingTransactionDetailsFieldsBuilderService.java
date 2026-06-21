bfd5025526689b9203adb2d72a562284fdf72772
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-06-21 19:20:54 +0200

    Update OnboardingTransactionDetailsFieldsBuilderService.java

--- OnboardingTransactionDetailsFieldsBuilderService.java
+++ OnboardingTransactionDetailsFieldsBuilderService.java
@@ -1,24 +1,124 @@
-FieldHelper.testFieldValueAndIncr(ANNUAL_INCOME + "_1", TextInputField.class, group, "N/A", order, null, false, false);
-FieldHelper.testFieldValueAndIncr(ANNUAL_INCOME + "_2", TextInputField.class, group, "N/A", order, null, false, false);
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+public class CountryOfWealthRiskTest {
 
-FieldHelper.testFieldValueAndIncr(TWENTY_PERCENT_INCOME + "_1", SelectInputField.class, group, NO, order, null, false, false);
-FieldHelper.testFieldValueAndIncr(SOURCE_OF_WEALTH + "_1", TextInputField.class, group, null, order, null, false, true);
+    static private final MockedStatic<ChecklistUtils> checklistUtils = Mockito.mockStatic(ChecklistUtils.class);
+    private Map<String,List<String>> risk;
+    private BusinessTransaction transaction;
 
-FieldHelper.testFieldValueAndIncr(TWENTY_PERCENT_INCOME + "_2", SelectInputField.class, group, NO, order, null, false, false);
-FieldHelper.testFieldValueAndIncr(SOURCE_OF_WEALTH + "_2", TextInputField.class, group, null, order, null, false, true);
+    @BeforeEach
+    void resetBeforeTest() {
 
-FieldHelper.testFieldValueAndIncr(MINIMUM_WEALTH + "_1", SelectInputField.class, group, null, order, null, true, true);
+        transaction = TransactionBuilderServiceHelper.createTransaction();
+        checklistUtils.reset();
+        risk = Map.of(BLOCKED, new ArrayList<>(), HIGH, new ArrayList<>());
+    }
 
-FieldHelper.testFieldValueAndIncr(WEALTH_ALLOCATION_OK + "_1", SelectInputField.class, group, NO, order, null, true, true);
+    @AfterAll
+    static void closeStaticMocks() {
 
-FieldHelper.testFieldValueAndIncr(WEALTH_ORIGINATING_COUNTRY_1_RISK + "_1", TextInputField.class, group, "", order, null, true, false);
-FieldHelper.testFieldValueAndIncr(WEALTH_ORIGINATING_COUNTRY_2_RISK + "_1", TextInputField.class, group, "", order, null, false, false);
-FieldHelper.testFieldValueAndIncr(WEALTH_ORIGINATING_COUNTRY_1_RISK + "_2", TextInputField.class, group, "", order, null, true, false);
-FieldHelper.testFieldValueAndIncr(WEALTH_ORIGINATING_COUNTRY_2_RISK + "_2", TextInputField.class, group, "", order, null, false, false);
+        checklistUtils.close();
+    }
 
-FieldHelper.testFieldValueAndIncr(WEALTH_ALLOCATION + "_1", TextInputField.class, group, null, order, null, true, true);
+    @ParameterizedTest
+    @MethodSource("highOkParameters")
+    void buildCountryOfWealthRisk_High_OK(String kycMandatory, String kycObtained, String sameOrigin, String newOrigin,
+            String ESC_RF_001RiskLevel) {
 
-FieldHelper.testFieldValueAndIncr(TOTAL_WEALTH + "_1", TextInputField.class, group, "N/A", order, null, false, false);
-FieldHelper.testFieldValueAndIncr(TOTAL_WEALTH + "_2", TextInputField.class, group, "N/A", order, null, false, false);
+        transaction.getRiskFactorResults().forEach(risk -> {
+            if (ESC_RF_001.equals(risk.getReference())) {
+                risk.setRiskLevel(ESC_RF_001RiskLevel);
+                risk.setData("ESC_RF_001 data");
+            }
+        });
+        ChecklistUtilsHelper.mockChecklistFieldValue(checklistUtils, KYC_MANDATORY, kycMandatory);
+        ChecklistUtilsHelper.mockChecklistFieldValue(checklistUtils, NEW_KYC_OBTAINED, kycObtained);
+        ChecklistUtilsHelper.mockChecklistFieldValue(checklistUtils, IS_SAME_ORIGIN_PREMIUM, sameOrigin);
+        ChecklistUtilsHelper.mockChecklistFieldValue(checklistUtils, NEW_ORIGIN_PREM_CLASS, newOrigin);
 
-FieldHelper.testFieldValueAndIncr(KYC_SUPPORTING_DOCUMENTS, SelectInputField.class, group, null, order, null, true, true);
+        String countryOfWealthData =
+                transaction.getRiskFactorResults().stream().filter(risk -> ESC_RF_001.equals(risk.getReference())).toList().getFirst().getData();
+        CaseRisk result = CountryOfWealthRisk.countryOfWealth(null, transaction, risk);
+        assertEquals(CaseRisk.CASE_RISK_HIGH, result);
+        assertEquals(1, risk.get(HIGH).size());
+        assertEquals(0, risk.get(BLOCKED).size());
+        assertEquals("EBO/Originator country of wealth generation : " + countryOfWealthData, risk.get(HIGH).getFirst());
+    }
+
+    @ParameterizedTest
+    @MethodSource("standardOkParameters")
+    void buildCountryOfWealthRisk_Standard_OK(String kycMandatory, String kycObtained, String sameOrigin, String newOrigin,
+            String ESC_RF_001RiskLevel) {
+
+        transaction.getRiskFactorResults().forEach(risk -> {
+            if (ESC_RF_001.equals(risk.getReference())) {
+                risk.setRiskLevel(ESC_RF_001RiskLevel);
+            }
+        });
+        ChecklistUtilsHelper.mockChecklistFieldValue(checklistUtils, KYC_MANDATORY, kycMandatory);
+        ChecklistUtilsHelper.mockChecklistFieldValue(checklistUtils, NEW_KYC_OBTAINED, kycObtained);
+        ChecklistUtilsHelper.mockChecklistFieldValue(checklistUtils, IS_SAME_ORIGIN_PREMIUM, sameOrigin);
+        ChecklistUtilsHelper.mockChecklistFieldValue(checklistUtils, NEW_ORIGIN_PREM_CLASS, newOrigin);
+
+        CaseRisk result = CountryOfWealthRisk.countryOfWealth(null, transaction, risk);
+        assertEquals(CaseRisk.CASE_RISK_STANDARD, result);
+        assertEquals(0, risk.get(HIGH).size());
+
+    }
+
+    @Test
+    void buildCountryOfWealthListRisk_Medium_OK() {
+
+        transaction.getRiskFactorResults().forEach(risk -> {
+            if (ESC_RF_001.equals(risk.getReference())) {
+                risk.setRiskLevel("Medium");
+            }
+        });
+        ChecklistUtilsHelper.mockChecklistFieldValue(checklistUtils, KYC_MANDATORY, YES);
+        ChecklistUtilsHelper.mockChecklistFieldValue(checklistUtils, NEW_KYC_OBTAINED, YES);
+        ChecklistUtilsHelper.mockChecklistFieldValue(checklistUtils, NEW_ORIGIN_PREM_CLASS, YES);
+
+        CaseRisk result = CountryOfWealthRisk.countryOfWealth(null, transaction, risk);
+        assertEquals(CaseRisk.CASE_RISK_MEDIUM, result);
+        assertEquals(0, risk.get(HIGH).size());
+        assertEquals(0, risk.get(BLOCKED).size());
+    }
+
+    @Test
+    void buildCountryOfWealthListRisk_MissingESCRF001_OK() {
+
+        transaction.getRiskFactorResults().forEach(risk -> {
+            if (ESC_RF_001.equals(risk.getReference())) {
+                risk.setData("Missing data");
+                risk.setRiskLevel("HIGH");
+            }
+        });
+        ChecklistUtilsHelper.mockChecklistFieldValue(checklistUtils, KYC_MANDATORY, YES);
+        ChecklistUtilsHelper.mockChecklistFieldValue(checklistUtils, NEW_KYC_OBTAINED, YES);
+        ChecklistUtilsHelper.mockChecklistFieldValue(checklistUtils, NEW_ORIGIN_PREM_CLASS, YES);
+
+        String countryOfWealthData =
+                transaction.getRiskFactorResults().stream().filter(risk -> ESC_RF_001.equals(risk.getReference())).toList().getFirst().getData();
+
+        CaseRisk result = CountryOfWealthRisk.countryOfWealth(null, transaction, risk);
+        assertEquals(CaseRisk.CASE_RISK_STANDARD, result);
+        assertEquals(0, risk.get(HIGH).size());
+        assertEquals(1, risk.get(BLOCKED).size());
+        assertEquals(countryOfWealthData, risk.get(BLOCKED).getFirst());
+    }
+
+    static Stream<Arguments> highOkParameters() {
+
+        return Stream.of(Arguments.of(YES, YES, YES, YES, HIGH), Arguments.of(YES, YES, YES, YES, VERY_HIGH),
+                Arguments.of(YES, YES, NO, YES, HIGH), Arguments.of(YES, YES, NO, YES, VERY_HIGH), Arguments.of(YES, NO, YES, NO, HIGH),
+                Arguments.of(NO, YES, YES, NO, HIGH), Arguments.of(NO, NO, YES, NO, HIGH), Arguments.of(NO, NO, NO, YES, HIGH),
+                Arguments.of(NO, NO, YES, YES, HIGH));
+    }
+
+    static Stream<Arguments> standardOkParameters() {
+
+        return Stream.of(Arguments.of(YES, YES, YES, NO, HIGH), Arguments.of(YES, YES, YES, NO, VERY_HIGH),
+                Arguments.of(YES, YES, YES, NO, MEDIUM), Arguments.of(YES, YES, YES, NO, STANDARD), Arguments.of(YES, YES, NO, YES, STANDARD),
+                Arguments.of(YES, YES, NO, NO, STANDARD), Arguments.of(NO, NO, NO, NO, STANDARD));
+    }
+}
