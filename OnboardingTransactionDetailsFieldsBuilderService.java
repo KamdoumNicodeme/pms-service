9758a943b3b382827493adb1b382e22334325364
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-06-21 17:32:42 +0200

    Refactor investment strategy resolution method

--- OnboardingTransactionDetailsFieldsBuilderService.java
+++ OnboardingTransactionDetailsFieldsBuilderService.java
@@ -1,30 +1,9 @@
-private String resolveInvestmentStrategyName(final WebFormGroup fund) {
-    var regularIma = Optional.ofNullable(getGroupInGroup(fund, INVESTMENT_STRATEGY))
-            .map(strategy -> getGroupInGroup(strategy, REGULAR_IMA))
-            .orElse(null);
-
-    if (regularIma == null) {
-        return "N/A";
-    }
-
-    String investmentStrategy = Optional.ofNullable(getFieldInGroup(regularIma, INVESTMENT_STRATEGY))
+private String resolveCustodianCountry(final WebFormGroup fund) {
+    return Optional.ofNullable(
+                    getGroupInGroup(fund, CUSTODIAN_BANK_DESIGNATION))
+            .map(custodian -> getGroupInGroup(custodian, ADDRESS))
+            .map(address -> getFieldInGroup(address, COUNTRY))
             .map(WebformUtils::getValueForWebFormField)
-            .orElse("");
-
-    if ("MANDATE".equals(investmentStrategy)) {
-        return Optional.ofNullable(getGroupInGroup(regularIma, INVESTMENT_MANDATE))
-                .map(mandate -> getFieldInGroup(mandate, STRATEGY_NAME))
-                .map(WebformUtils::getValueForWebFormField)
-                .filter(StringUtils::isNotBlank)
-                .orElse("N/A");
-    }
-
-    if ("DIFFERENT".equals(investmentStrategy)) {
-        return Optional.ofNullable(getFieldInGroup(regularIma, DIFFERENT_STRATEGY_NAME))
-                .map(WebformUtils::getValueForWebFormField)
-                .filter(StringUtils::isNotBlank)
-                .orElse("N/A");
-    }
-
-    return "N/A";
+            .filter(StringUtils::isNotBlank)
+            .orElse("N/A");
 }
