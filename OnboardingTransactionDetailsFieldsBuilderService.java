b77cd370eb6820c5301146a05454715e53503431
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-06-21 17:24:27 +0200

    Update OnboardingTransactionDetailsFieldsBuilderService.java

--- OnboardingTransactionDetailsFieldsBuilderService.java
+++ OnboardingTransactionDetailsFieldsBuilderService.java
@@ -1,38 +1,30 @@
-private String resolveEsgMandateStatus(
-        WebFormGroup internalDedicatedFund) {
-
-    var regularIma = Optional.ofNullable(
-            getGroupInGroup(
-                    getGroupInGroup(
-                            internalDedicatedFund,
-                            INVESTMENT_STRATEGY),
-                    REGULAR_IMA))
+private String resolveInvestmentStrategyName(final WebFormGroup fund) {
+    var regularIma = Optional.ofNullable(getGroupInGroup(fund, INVESTMENT_STRATEGY))
+            .map(strategy -> getGroupInGroup(strategy, REGULAR_IMA))
             .orElse(null);
 
     if (regularIma == null) {
-        return "NO";
+        return "N/A";
     }
 
-    String investmentStrategy =
-            getValueForWebFormField(
-                    getFieldInGroup(
-                            regularIma,
-                            INVESTMENT_STRATEGY));
-
-    String promoted =
-            getValueForWebFormField(
-                    getFieldInGroup(
-                            regularIma,
-                            IS_STRATEGY_PROMOTED));
+    String investmentStrategy = Optional.ofNullable(getFieldInGroup(regularIma, INVESTMENT_STRATEGY))
+            .map(WebformUtils::getValueForWebFormField)
+            .orElse("");
 
     if ("MANDATE".equals(investmentStrategy)) {
-        return "YES_PRE_VALIDATED";
+        return Optional.ofNullable(getGroupInGroup(regularIma, INVESTMENT_MANDATE))
+                .map(mandate -> getFieldInGroup(mandate, STRATEGY_NAME))
+                .map(WebformUtils::getValueForWebFormField)
+                .filter(StringUtils::isNotBlank)
+                .orElse("N/A");
     }
 
-    if ("DIFFERENT".equals(investmentStrategy)
-            && Boolean.parseBoolean(promoted)) {
-        return "YES_TO_BE_REVIEWED";
+    if ("DIFFERENT".equals(investmentStrategy)) {
+        return Optional.ofNullable(getFieldInGroup(regularIma, DIFFERENT_STRATEGY_NAME))
+                .map(WebformUtils::getValueForWebFormField)
+                .filter(StringUtils::isNotBlank)
+                .orElse("N/A");
     }
 
-    return "NO";
+    return "N/A";
 }
