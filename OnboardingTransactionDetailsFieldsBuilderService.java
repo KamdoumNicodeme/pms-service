82dd23a12aaaeb69e87bf225b33dba4b3cdaaf3e
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-06-21 18:16:54 +0200

    Update OnboardingTransactionDetailsFieldsBuilderService.java

--- OnboardingTransactionDetailsFieldsBuilderService.java
+++ OnboardingTransactionDetailsFieldsBuilderService.java
@@ -1,7 +1,38 @@
-private String buildMrlTrusteeDisplayIf(final ScreenDescription screenDescription) {
-    return ChecklistUtils.getFieldsById(screenDescription, THIRD_PARTY_SUB_TYPE_PH)
-            .stream()
-            .map(Field::getFieldId)
-            .map(fieldId -> "#" + fieldId + "# == \"Trust\"")
-            .collect(Collectors.joining(" || "));
+private void evaluatePhTypeAssessment(
+        Group group,
+        BusinessTransaction transaction,
+        Map<String, List<String>> overallCaseRisk) {
+
+    var phTypeAssessment = (TextInputField) ChecklistUtils.getFieldInGroup(group, PH_TYPE_ASSESSMENT);
+
+    if (phTypeAssessment == null) {
+        phTypeAssessment = TextInputField.builder()
+                .fieldId(PH_TYPE_ASSESSMENT)
+                .build();
+        group.getFields().add(phTypeAssessment);
+    }
+
+    phTypeAssessment.setIsActive(true);
+    phTypeAssessment.incrementOrder();
+    phTypeAssessment.setLabel("PH type assessment");
+    phTypeAssessment.setEnabled(false);
+    phTypeAssessment.setMandatory(true);
+    phTypeAssessment.setDisplayIf("true");
+    phTypeAssessment.setLabelBold(false);
+    phTypeAssessment.setSourceSystem("From CLASS");
+
+    String phType = RulesUtils.getRiskFactorData(transaction, "INT_RF_014");
+
+    if (phType.contains("N/A")) {
+        var blockedPhTypes = Arrays.stream(phType.split(";"))
+                .map(String::trim)
+                .filter(type -> !"N/A".equals(type))
+                .toList();
+
+        overallCaseRisk
+                .computeIfAbsent(RulesConstants.BLOCKED, key -> new ArrayList<>())
+                .add("PH Type risk factor cannot be assessed - " + String.join(",", blockedPhTypes));
+    } else {
+        phTypeAssessment.setSelectedValue(phType);
+    }
 }
