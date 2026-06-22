d65f1793bbe261d50a8849cf8f0e02aeab119054
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-06-22 07:59:08 +0200

    Update OnboardingTransactionDetailsFieldsBuilderService.java

--- OnboardingTransactionDetailsFieldsBuilderService.java
+++ OnboardingTransactionDetailsFieldsBuilderService.java
@@ -1,6 +1,6 @@
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
-class PhIndividualNotEboRiskTest {
+class ThirdPartyPaymentNoEvidenceLegalRiskTest {
 
     private static final MockedStatic<ChecklistUtils> checklistUtils =
             Mockito.mockStatic(ChecklistUtils.class);
@@ -26,31 +26,68 @@ class PhIndividualNotEboRiskTest {
     }
 
     @Test
-    void individualNotEbo_High_OK() {
-        ScreenDescription sd = createScreenDescription(null, "PH Type=Physical", YES);
+    void thirdPartyLegalNoEvidence_High_OK() {
+        ScreenDescription sd = createScreenDescription(
+                null,
+                "true",
+                YES,
+                NO
+        );
 
-        CaseRisk result = PhIndividualNotEboRisk.individualNotEbo(sd, overallCaseRisk);
+        CaseRisk result =
+                ThirdPartyPaymentNoEvidenceLegalRisk.thirdPartyLegalNoEvidence(sd, overallCaseRisk);
 
         assertEquals(CaseRisk.CASE_RISK_HIGH, result);
         assertEquals(1, overallCaseRisk.get(HIGH).size());
-        assertEquals("PH is an individual and is not the EBO", overallCaseRisk.get(HIGH).getFirst());
+        assertEquals(
+                "3rd party payment - No evidence that the legal entity is known by tax authorities",
+                overallCaseRisk.get(HIGH).getFirst()
+        );
     }
 
     @Test
-    void individualNotEbo_Standard_WhenPhNotPhysical_OK() {
-        ScreenDescription sd = createScreenDescription(null, "PH Type=Moral", YES);
+    void thirdPartyLegalNoEvidence_Standard_WhenPayerNotLocatedNotDisplayed_OK() {
+        ScreenDescription sd = createScreenDescription(
+                null,
+                "false",
+                YES,
+                NO
+        );
 
-        CaseRisk result = PhIndividualNotEboRisk.individualNotEbo(sd, overallCaseRisk);
+        CaseRisk result =
+                ThirdPartyPaymentNoEvidenceLegalRisk.thirdPartyLegalNoEvidence(sd, overallCaseRisk);
 
         assertEquals(CaseRisk.CASE_RISK_STANDARD, result);
         assertEquals(0, overallCaseRisk.get(HIGH).size());
     }
 
     @Test
-    void individualNotEbo_Standard_WhenPhDifferentToEboNo_OK() {
-        ScreenDescription sd = createScreenDescription(null, "PH Type=Physical", NO);
+    void thirdPartyLegalNoEvidence_Standard_WhenPayerNotLocatedNo_OK() {
+        ScreenDescription sd = createScreenDescription(
+                null,
+                "true",
+                NO,
+                NO
+        );
 
-        CaseRisk result = PhIndividualNotEboRisk.individualNotEbo(sd, overallCaseRisk);
+        CaseRisk result =
+                ThirdPartyPaymentNoEvidenceLegalRisk.thirdPartyLegalNoEvidence(sd, overallCaseRisk);
+
+        assertEquals(CaseRisk.CASE_RISK_STANDARD, result);
+        assertEquals(0, overallCaseRisk.get(HIGH).size());
+    }
+
+    @Test
+    void thirdPartyLegalNoEvidence_Standard_WhenEvidenceYes_OK() {
+        ScreenDescription sd = createScreenDescription(
+                null,
+                "true",
+                YES,
+                YES
+        );
+
+        CaseRisk result =
+                ThirdPartyPaymentNoEvidenceLegalRisk.thirdPartyLegalNoEvidence(sd, overallCaseRisk);
 
         assertEquals(CaseRisk.CASE_RISK_STANDARD, result);
         assertEquals(0, overallCaseRisk.get(HIGH).size());
@@ -58,10 +95,16 @@ class PhIndividualNotEboRiskTest {
 
     @ParameterizedTest
     @ValueSource(strings = { HIGH, MEDIUM, STANDARD })
-    void individualNotEbo_ForcedRiskFromRiskValue_OK(String caseValue) {
-        ScreenDescription sd = createScreenDescription(caseValue, "PH Type=Physical", YES);
+    void thirdPartyLegalNoEvidence_ForcedRiskFromRiskValue_OK(String caseValue) {
+        ScreenDescription sd = createScreenDescription(
+                caseValue,
+                "true",
+                YES,
+                NO
+        );
 
-        CaseRisk result = PhIndividualNotEboRisk.individualNotEbo(sd, overallCaseRisk);
+        CaseRisk result =
+                ThirdPartyPaymentNoEvidenceLegalRisk.thirdPartyLegalNoEvidence(sd, overallCaseRisk);
 
         assertEquals(RulesUtils.resolveForcedCaseRisk(caseValue), result);
         assertEquals(0, overallCaseRisk.get(HIGH).size());
@@ -70,8 +113,9 @@ class PhIndividualNotEboRiskTest {
 
     private ScreenDescription createScreenDescription(
             String riskValue,
-            String phType,
-            String phDifferentToEbo
+            String payerNotLocatedDisplayIf,
+            String payerNotLocated,
+            String evidence
     ) {
         List<Field> fields = new ArrayList<>();
 
@@ -80,14 +124,15 @@ class PhIndividualNotEboRiskTest {
                 .selectedValue(riskValue)
                 .build());
 
-        fields.add(TextInputField.builder()
-                .fieldId(PH_TYPE_ASSESSMENT)
-                .selectedValue(phType)
+        fields.add(SelectInputField.builder()
+                .fieldId(PAYER_NOT_LOCATED)
+                .displayIf(payerNotLocatedDisplayIf)
+                .selectedValue(payerNotLocated)
                 .build());
 
         fields.add(SelectInputField.builder()
-                .fieldId(PH_DIFFERENT_TO_EBO)
-                .selectedValue(phDifferentToEbo)
+                .fieldId(EVIDENCE_LEGAL_ENTITY)
+                .selectedValue(evidence)
                 .build());
 
         Group group = Group.builder()
