77ab9733711ec6d7fe1126ae94625c032fceb040
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-07-13 08:16:20 +0200

    Update OnboardingTransactionDetailsFieldsBuilderService.java

--- OnboardingTransactionDetailsFieldsBuilderService.java
+++ OnboardingTransactionDetailsFieldsBuilderService.java
@@ -1,1 +1,85 @@
-Il ne s'agit pas d'un bug actif dans le système. Le dossier concerné est un cas ancien : son contrôle était clôturé depuis le 4 septembre 2025 et il a donc été évalué sous une version antérieure de la règle d'escalade. Le résultat observé (medium) reflète la logique telle qu'elle était à cette date, et non le comportement actuel.
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdditionInvestmentsWebForm implements IConnectWebFormGroup {
+
+    @Valid
+    private List<AdditionFasWebForm> specialisedAssuranceFunds;
+    @Valid
+    private List<AdditionIdfWebForm> internalDedicatedFunds;
+    @Valid
+    private List<AdditionFdfWebForm> familyDedicatedFunds;
+    @Valid
+    private AdditionExternalCollectiveFundWebForm externalCollectiveFund;
+    @Valid
+    private AdditionInternalCollectiveFundWebForm internalCollectiveFunds;
+}
+
+
+
+public class AdditionInternalFundPositionWebForm implements IConnectWebFormGroup {
+
+    @Valid
+    @NotNull(message = MANDATORY_FIELD_VALUE_MISSING)
+    private AdditionInternalFundIdWebForm id;
+    @NotNull(message = MANDATORY_FIELD_VALUE_MISSING)
+    private String fundName;
+    @NotNull(message = MANDATORY_FIELD_VALUE_MISSING)
+    private String fundMnemonic;
+    private String countryOfApplicableLaw; // Contrary to ECF, we don't have this information for ICF
+    private String fundStatus; // Contrary to ECF, we don't have this information for ICF (might default to IN_FORCE ?)
+    private String fundIssuer; // Contrary to ECF, we don't have this information fo ICF (might default to LIA ?)
+    @NotNull(message = MANDATORY_FIELD_VALUE_MISSING)
+    private String lastPriceDate;
+    @NotNull(message = MANDATORY_FIELD_VALUE_MISSING)
+    private String lastPriceValue;
+    @NotNull(message = MANDATORY_FIELD_VALUE_MISSING)
+    private Integer levelRisk;
+    private Integer fundSrri; // Contrary to ECF, we don't have this information for ICF
+    private BigDecimal currentEstimatedFundCCY;
+    private BigDecimal currentEstimatedFundEUR;
+    private BigDecimal estimatedFundCCY;
+    private BigDecimal estimatedFundEUR;
+    @NotNull(message = MANDATORY_FIELD_VALUE_MISSING)
+    @Min(value = 0, message = PERCENTAGE_UNDER_0)
+    @Max(value = 100, message = PERCENTAGE_OVER_100)
+    private BigDecimal percent;
+    @NotNull(message = MANDATORY_FIELD_VALUE_MISSING)
+    @ReferenceDataExists(domain = DOMAIN_ADDITION_CURRENCY, emptyAllowed = false)
+    private String currency;
+    private BigDecimal rateChangeFundCcy;
+}
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@AdditionInternalCollectiveFundIsValid
+public class AdditionInternalCollectiveFundWebForm implements IConnectWebFormGroup {
+
+    @NotNull(message = MANDATORY_FIELD_VALUE_MISSING)
+    private AdditionBasicsWebForm basics;
+    @Valid
+    private List<AdditionInternalFundWebForm> internalFunds = new ArrayList<AdditionInternalFundWebForm>();
+}
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdditionInternalFundWebForm implements IConnectWebFormGroup {
+
+    @NotNull(message = MANDATORY_FIELD_VALUE_MISSING)
+    private AdditionBasicsWebForm basics;
+    private String fundsGroupId;
+    private String fundName;
+    private String productComponentNumber;
+    @Valid
+    private AdditionInternalFundPositionWebForm internalFundPosition;
+
+}
+
+
+
