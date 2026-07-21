136844562083796dd9349c3ab726addb1a17d131
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-07-21 10:29:29 +0200

    Update OnboardingTransactionDetailsFieldsBuilderService.java

--- OnboardingTransactionDetailsFieldsBuilderService.java
+++ OnboardingTransactionDetailsFieldsBuilderService.java
@@ -1,1 +1,3 @@
-The backend shall expose the current client information required by the Change of Client Information task.
+Context
+
+As part of the Change of Client Information (CCI) workflow, users must be able to review the current information stored in CLASS before validating or updating the client information.
