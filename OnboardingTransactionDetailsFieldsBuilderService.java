aca8bf7f1e6469c1aa50ca03b9e726d273be4f55
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-07-21 10:15:39 +0200

    Update OnboardingTransactionDetailsFieldsBuilderService.java

--- OnboardingTransactionDetailsFieldsBuilderService.java
+++ OnboardingTransactionDetailsFieldsBuilderService.java
@@ -1,5 +1,5 @@
-The Change of Client Information (CCI) workflow requires an internal domain model to manipulate client information throughout the workflow.
+As part of the replacement of the Case Management Tool (CMT), the Change of Client Information (CCI) workflow requires the Client Profiling Application to display the current client information stored in CLASS.
 
-Although the CCI model is structurally very close to the CLASS model, it must remain independent from it. CLASS will remain the system of record and dedicated mappers will be responsible for converting between both models.
+The “Change of Client Information” task allows users to compare the current information stored in CLASS with the information submitted during the Digital Fact Find.
 
-The CCI model will become the canonical model used across the application (Camunda process variables, REST APIs, business services and UI).
+To support this functionality, the CLIP backend must expose the current Policy Holder information retrieved from CLASS.
