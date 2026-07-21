1782d0647176120faa4559bf9b0cf89585e28bb6
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-07-21 12:01:38 +0200

    Update OnboardingTransactionDetailsFieldsBuilderService.java

--- OnboardingTransactionDetailsFieldsBuilderService.java
+++ OnboardingTransactionDetailsFieldsBuilderService.java
@@ -1,52 +1,33 @@
 Context
 
-The Change of Client Information (CCI) workflow requires the Client Profiling Application to display the current policy correspondence information stored in CLASS.
+As part of the Change of Client Information (CCI) workflow, the Policy Servicing Administrator (PSA) or PCS user must be able to review and update the client information submitted during the Digital Fact Find.
 
-The “Change of Client Information” task allows users to compare the current policy correspondence stored in CLASS with the information submitted during the Digital Fact Find.
+The user input must be persisted during the execution of the task and be available until the task is completed.
 
-To support this functionality, the CLIP backend must expose the current policy correspondence information retrieved from CLASS.
+The CCI worker shall expose REST APIs allowing the frontend to retrieve, update and complete the user task.
 
 ⸻
 
 Description
 
-The backend shall expose the current policy correspondence information required by the Change of Client Information task.
+The user edits the information displayed on the right section of the Change of Client Information screen.
 
-The exposed data will populate the read-only (left) section of the comparison screen.
+The worker shall expose APIs allowing the frontend to:
 
-The following information must be provided.
+* retrieve the current user input;
+* save the current modifications;
+* complete the Change of Client Information task.
 
-⸻
+The user input is stored in the CCI worker data and will later be used to update CLASS.
 
-Sending Address & Communication Preferences
+The following sections are concerned:
 
-The backend shall expose:
-
-Correspondence Address
-
-* Street
-* Number
-* House Name
-* Apartment Number
-* City
-* Postcode
-* County
-* Area
-* Country
-
-⸻
-
-Communication Preferences
-
-The backend shall expose:
-
-* Language
-* Consent to receive electronic communication
-
-⸻
-
-Policy Communication
-
-The backend shall expose:
-
-* Policy Opt In / Out
+* Policy Correspondence
+* Physical Person
+* Legal Entity
+* Controlling Persons
+* Contact Details
+* Identity Documents
+* Tax Information
+* Communication Preferences
+* Opt In / Out
