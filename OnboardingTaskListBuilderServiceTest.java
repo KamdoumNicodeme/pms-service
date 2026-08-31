b84fce0730c56ffbad1e852087e1dd87781eae42
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-08-31 18:41:41 +0200

    Update OnboardingTaskListBuilderServiceTest.java

--- OnboardingTaskListBuilderServiceTest.java
+++ OnboardingTaskListBuilderServiceTest.java
@@ -1,50 +1,70 @@
-private addressGroup(
-  key: string,
-  label: string,
-  address: IAddress | null | undefined
-): ComparisonGroupFieldDto {
-
-  return {
-    key,
-    label,
-    kind: 'group',
-
-    lines: [
-      ['no', 'address'],
-      ['postCode', 'town'],
-      ['country'],
-    ],
-
-    fields: [
-      this.scalarField(
-        'no',
-        'Number',
-        address?.no
-      ),
-
-      this.scalarField(
-        'address',
-        'Street',
-        address?.address
-      ),
-
-      this.scalarField(
-        'postCode',
-        'Post code',
-        address?.postCode
-      ),
-
-      this.scalarField(
-        'town',
-        'Town',
-        address?.town
-      ),
-
-      this.scalarField(
-        'country',
-        'Country',
-        this.normalizeCountry(address?.country)
-      ),
-    ],
-  };
+{
+  "policy": {
+    "policyNumber": "2201-123456",
+    "type": "LIFE_INSURANCE",
+    "currency": "EUR",
+    "broker": "B/12345",
+    "brokerName": "PSA - Policy Servicing Administration",
+    "brokerTargetMarket": "FRANCE",
+    "countryOfLaw": "FR",
+    "countryOfBusinessOrigin": "FR",
+    "taxCountry": "FR",
+    "digitalConsent": {
+      "receiveElectronicCommunicationsConsentStatus": "YES",
+      "receiveElectronicCommunicationsConsentFromDate": "2026-06-26T10:30:00Z"
+    },
+    "clients": [
+      {
+        "type": "PHYSICAL",
+        "thirdPartyId": "00012569",
+        "aeoiStatus": "COMPLIANT",
+        "fatcaStatus": "NOT_US_PERSON",
+        "taxCountry": "FR",
+        "taxNumber": "123456789",
+        "roleTypes": [
+          "POLICY_HOLDER"
+        ],
+        "countryOfResidence": "FR",
+        "name": null,
+        "companyLegalForm": null,
+        "lastname": "Dupont",
+        "firstName": "Paul",
+        "idDocumentExpirationDate": "2030-06-26T00:00:00Z"
+      },
+      {
+        "type": "PHYSICAL",
+        "thirdPartyId": "00035698",
+        "aeoiStatus": "COMPLIANT",
+        "fatcaStatus": "NOT_US_PERSON",
+        "taxCountry": "LU",
+        "taxNumber": "LU987654321",
+        "roleTypes": [
+          "POLICY_HOLDER"
+        ],
+        "countryOfResidence": "LU",
+        "name": null,
+        "companyLegalForm": null,
+        "lastname": "Dunn",
+        "firstName": "Josefina",
+        "idDocumentExpirationDate": "2029-12-31T00:00:00Z"
+      },
+      {
+        "type": "MORAL",
+        "thirdPartyId": "00045678",
+        "aeoiStatus": "COMPLIANT",
+        "fatcaStatus": "NOT_US_PERSON",
+        "taxCountry": "LU",
+        "taxNumber": "LU123456789",
+        "roleTypes": [
+          "POLICY_HOLDER"
+        ],
+        "countryOfResidence": "LU",
+        "name": "ABC Holding S.A.",
+        "companyLegalForm": "SA",
+        "lastname": null,
+        "firstName": null,
+        "idDocumentExpirationDate": null
+      }
+    ]
+  }
 }
