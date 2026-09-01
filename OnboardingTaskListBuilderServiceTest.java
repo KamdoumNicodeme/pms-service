fefd159c85f8aaa825457bc1eb089dcb5ea9449a
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-09-01 11:09:00 +0200

    Update OnboardingTaskListBuilderServiceTest.java

--- OnboardingTaskListBuilderServiceTest.java
+++ OnboardingTaskListBuilderServiceTest.java
@@ -1,70 +1,55 @@
-{
-  "policy": {
-    "policyNumber": "2201-123456",
-    "type": "LIFE_INSURANCE",
-    "currency": "EUR",
-    "broker": "B/12345",
-    "brokerName": "PSA - Policy Servicing Administration",
-    "brokerTargetMarket": "FRANCE",
-    "countryOfLaw": "FR",
-    "countryOfBusinessOrigin": "FR",
-    "taxCountry": "FR",
-    "digitalConsent": {
-      "receiveElectronicCommunicationsConsentStatus": "YES",
-      "receiveElectronicCommunicationsConsentFromDate": "2026-06-26T10:30:00Z"
-    },
-    "clients": [
-      {
-        "type": "PHYSICAL",
-        "thirdPartyId": "00012569",
-        "aeoiStatus": "COMPLIANT",
-        "fatcaStatus": "NOT_US_PERSON",
-        "taxCountry": "FR",
-        "taxNumber": "123456789",
-        "roleTypes": [
-          "POLICY_HOLDER"
-        ],
-        "countryOfResidence": "FR",
-        "name": null,
-        "companyLegalForm": null,
-        "lastname": "Dupont",
-        "firstName": "Paul",
-        "idDocumentExpirationDate": "2030-06-26T00:00:00Z"
-      },
-      {
-        "type": "PHYSICAL",
-        "thirdPartyId": "00035698",
-        "aeoiStatus": "COMPLIANT",
-        "fatcaStatus": "NOT_US_PERSON",
-        "taxCountry": "LU",
-        "taxNumber": "LU987654321",
-        "roleTypes": [
-          "POLICY_HOLDER"
-        ],
-        "countryOfResidence": "LU",
-        "name": null,
-        "companyLegalForm": null,
-        "lastname": "Dunn",
-        "firstName": "Josefina",
-        "idDocumentExpirationDate": "2029-12-31T00:00:00Z"
-      },
-      {
-        "type": "MORAL",
-        "thirdPartyId": "00045678",
-        "aeoiStatus": "COMPLIANT",
-        "fatcaStatus": "NOT_US_PERSON",
-        "taxCountry": "LU",
-        "taxNumber": "LU123456789",
-        "roleTypes": [
-          "POLICY_HOLDER"
-        ],
-        "countryOfResidence": "LU",
-        "name": "ABC Holding S.A.",
-        "companyLegalForm": "SA",
-        "lastname": null,
-        "firstName": null,
-        "idDocumentExpirationDate": null
-      }
-    ]
+private professionalFields(
+  holder: IPhysicalPerson
+): readonly ComparisonFieldDto[] {
+
+  const professionalDetails = holder.professionalDetails;
+
+  if (!professionalDetails) {
+    return [];
   }
+
+  const fields: ComparisonFieldDto[] = [
+    this.selectField(
+      'profession',
+      'Profession',
+      professionalDetails.profession,
+      professionOptions
+    ),
+  ];
+
+  const unemployed =
+    professionalDetails.profession === 'UNEMPLOYED';
+
+  if (!unemployed) {
+    fields.push(
+      this.selectField(
+        'profession-status',
+        'Profession status',
+        professionalDetails.status,
+        professionStatusOptions
+      ),
+
+      this.scalarField(
+        'employer-name',
+        'Employer Name',
+        professionalDetails.companyName
+      ),
+
+      this.selectField(
+        'employer-country',
+        'Employer Country',
+        professionalDetails.companyCountry,
+        countryOptions
+      ),
+
+      this.selectField(
+        'industry-sector',
+        'Industry Sector',
+        professionalDetails.sector,
+        industrySectorOptions
+      )
+    );
+  }
+
+  return fields;
 }
