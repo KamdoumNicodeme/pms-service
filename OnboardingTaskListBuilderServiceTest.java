91bbe04f7109b605069ba1d5dfb35e35a0d60037
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-09-10 12:04:05 +0200

    Update OnboardingTaskListBuilderServiceTest.java

--- OnboardingTaskListBuilderServiceTest.java
+++ OnboardingTaskListBuilderServiceTest.java
@@ -1,34 +1,130 @@
-@else if (row.kind === 'list') {
+<div class="list__add">
 
-  <comparison-list-field
-    [row]="row"
+  @if ((row().entryFields?.length ?? 0) > 0) {
 
-    [expandedId]="store.expandedId()"
+    <button
+      type="button"
+      class="list__add__trigger"
+      (click)="openStructuredModal()"
+    >
+      <nz-icon nzType="plus" />
+      Add {{ row().entryNoun }}
+    </button>
 
-    [focusedId]="store.focusedId()"
+  } @else {
 
-    (toggleEntry)="store.toggleExpanded($event)"
+    @if (adding()) {
 
-    (resetEntry)="store.reset($event)"
+      <input
+        nz-input
+        class="list__add__input"
+        [placeholder]="'New ' + row().entryNoun"
+        [ngModel]="draft()"
+        (ngModelChange)="draft.set($event)"
+        (keydown.enter)="submit()"
+        (keydown.escape)="cancelAdd()"
+      />
 
-    (focusEntry)="store.focus($event)"
+      <button
+        type="button"
+        class="list__add__confirm"
+        [disabled]="draft().trim() === ''"
+        (click)="submit()"
+      >
+        Add
+      </button>
 
-    (applyEntry)="onApplyEntry($event)"
+      <button
+        type="button"
+        class="list__add__cancel"
+        (click)="cancelAdd()"
+      >
+        Cancel
+      </button>
 
-    (addEntry)="store.addManualEntry(
-      row.key,
-      $event
-    )"
+    } @else {
 
-    (addStructuredEntry)="store.addManualStructuredEntry(
-      row.key,
-      $event.fields
-    )"
+      <button
+        type="button"
+        class="list__add__trigger"
+        (click)="adding.set(true)"
+      >
+        <nz-icon nzType="plus" />
+        Add {{ row().entryNoun }}
+      </button>
 
-    (removeEntry)="store.removeManualEntry(
-      row.key,
-      $event
-    )"
-  />
+    }
 
-}
+  }
+
+</div>
+
+
+<nz-modal
+  [nzVisible]="structuredModalVisible()"
+  nzTitle="Add tax information"
+  nzOkText="Add"
+  nzCancelText="Cancel"
+  [nzOkDisabled]="!canSubmitStructured()"
+  (nzOnOk)="submitStructured()"
+  (nzOnCancel)="closeStructuredModal()"
+>
+
+  <ng-container *nzModalContent>
+
+    <div class="tax-form">
+
+      @for (
+        field of row().entryFields ?? [];
+        track field.key
+      ) {
+
+        <div class="tax-form__field">
+
+          <label class="tax-form__label">
+            {{ field.label }}
+          </label>
+
+          @if (field.kind === 'select') {
+
+            <nz-select
+              class="tax-form__control"
+              [ngModel]="fieldValue(field.key)"
+              (ngModelChange)="setFieldValue(field.key, $event)"
+              nzPlaceHolder="Select a value"
+            >
+
+              @for (
+                option of field.options ?? [];
+                track option.value
+              ) {
+
+                <nz-option
+                  [nzValue]="option.value"
+                  [nzLabel]="option.label"
+                />
+
+              }
+
+            </nz-select>
+
+          } @else {
+
+            <input
+              nz-input
+              class="tax-form__control"
+              [ngModel]="fieldValue(field.key)"
+              (ngModelChange)="setFieldValue(field.key, $event)"
+            />
+
+          }
+
+        </div>
+
+      }
+
+    </div>
+
+  </ng-container>
+
+</nz-modal>
