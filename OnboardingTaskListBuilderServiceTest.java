70481e171fd96e75bc761905bea8b4c2d386064f
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-09-11 12:50:29 +0200

    Update OnboardingTaskListBuilderServiceTest.java

--- OnboardingTaskListBuilderServiceTest.java
+++ OnboardingTaskListBuilderServiceTest.java
@@ -1,64 +1,13 @@
-findReferencesByDomains(
-  domains: string[]
-): Observable<Record<string, IReference[]>> {
+constructor() {
+  console.log('CoreComparisonDataService CREATED');
 
-  return this.#httpClient.get<
-    Record<string, IReference[]>
-  >(
-    `${environment.apiUrls.get('cma')}/references`,
-    {
-      params: {
-        domains: domains.join(','),
-      },
-    }
-  );
-}
+  try {
+    console.log('BEFORE loadReferences');
 
-private loadReferences(): void {
-  this.#referenceService
-    .findReferencesByDomains([
-      'FATCA_STATUS',
-      'AEOI_STATUS',
-    ])
-    .subscribe({
-      next: (
-        references: Record<string, IReference[]>
-      ): void => {
+    this.loadReferences();
 
-        this.FATCA_STATUS_OPTIONS.set(
-          references['FATCA_STATUS'] ?? []
-        );
-
-        this.AEOI_STATUS_OPTIONS.set(
-          references['AEOI_STATUS'] ?? []
-        );
-
-        console.log(
-          'FATCA REFERENCES',
-          this.FATCA_STATUS_OPTIONS()
-        );
-
-        console.log(
-          'AEOI REFERENCES',
-          this.AEOI_STATUS_OPTIONS()
-        );
-
-        console.log(
-          'FATCA OPTIONS',
-          this.fatcaOptions()
-        );
-
-        console.log(
-          'CRS OPTIONS',
-          this.crsOptions()
-        );
-      },
-
-      error: error => {
-        console.error(
-          'Unable to load references',
-          error
-        );
-      },
-    });
+    console.log('AFTER loadReferences');
+  } catch (error) {
+    console.error('ERROR CALLING loadReferences', error);
+  }
 }
