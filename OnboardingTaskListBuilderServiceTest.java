517983523fde32b4f15669f8fb9778a97e6a65b1
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-09-11 15:25:28 +0200

    Update OnboardingTaskListBuilderServiceTest.java

--- OnboardingTaskListBuilderServiceTest.java
+++ OnboardingTaskListBuilderServiceTest.java
@@ -1,4 +1,17 @@
-openSimpleAdd(): void {
-  this.draft.set('');
-  this.adding.set(true);
-}
+readonly options =
+  input<readonly ComparisonOption[]>([]);
+
+readonly displayValue = computed(() => {
+  const value = this.resolution().value;
+
+  if (!value) {
+    return null;
+  }
+
+  const option =
+    this.options().find(
+      option => option.value === value
+    );
+
+  return option?.label ?? value;
+});
