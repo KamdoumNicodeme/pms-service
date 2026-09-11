15665fe8a829198594defb8b29bddc56c4d6c69e
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-09-11 11:33:40 +0200

    Update OnboardingTaskListBuilderServiceTest.java

--- OnboardingTaskListBuilderServiceTest.java
+++ OnboardingTaskListBuilderServiceTest.java
@@ -1,13 +1,71 @@
-protected readonly fatcaOptions = computed<ComparisonOption[]>(() =>
-  this.FATCA_STATUS_OPTIONS().map((ref: IReference) => ({
-    value: ref.code,
-    label: ref.label,
-  }))
-);
+constructor() {
 
-protected readonly crsOptions = computed<ComparisonOption[]>(() =>
-  this.AEOI_STATUS_OPTIONS().map((ref: IReference) => ({
-    value: ref.code,
-    label: ref.label,
-  }))
-);
+    this.loadReferences();
+
+  }
+
+  private loadReferences(): void {
+
+    this.referenceService
+
+      .findReferencesByDomains([
+
+        'POLICY_TYPE',
+
+        'THIRD_PARTY_TYPE',
+
+        'FATCA_STATUS',
+
+        'AEOI_STATUS',
+
+      ])
+
+      .subscribe({
+
+        next: (value: IReference[]) => {
+
+          const refs =
+
+            new Map(
+
+              value.map(
+
+                (ref: IReference) => [
+
+                  ref.domain,
+
+                  ref
+
+                ]
+
+              )
+
+            );
+
+          this.POLICY_TYPES_OPTIONS.set(
+
+            refs.get('POLICY_TYPE') ?? []
+
+          );
+
+          this.THIRD_PARTY_TYPES_OPTIONS.set(
+
+            refs.get('THIRD_PARTY_TYPE') ?? []
+
+          );
+
+          this.FATCA_STATUS_OPTIONS.set(
+
+            refs.get('FATCA_STATUS') ?? []
+
+          );
+
+          this.AEOI_STATUS_OPTIONS.set(
+
+            refs.get('AEOI_STATUS') ?? []
+
+          );
+
+        },
+
+      });
