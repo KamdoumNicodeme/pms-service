65af4052137c46f83bec4b06178771c5dc3ccc1d
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-09-15 12:27:31 +0200

    Update OnboardingTaskListBuilderServiceTest.java

--- OnboardingTaskListBuilderServiceTest.java
+++ OnboardingTaskListBuilderServiceTest.java
@@ -1,1 +1,1 @@
-/tabs/item[tabId='CHECKLIST']/groups/item/fields/item[fieldId='IS_IT_THE_LAST_INSTALLMENT']/selectedValue
+/tabs/item[tabId='CHECKLIST']/groups/item[groupId='GENERAL_DETAILS']/fields/item[fieldId='IS_IT_THE_LAST_INSTALLMENT']/selectedValue
