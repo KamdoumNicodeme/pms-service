b4d864899783f409bbf51c8a93a6a03edd8ff7cb
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-09-17 17:37:18 +0200

    Update OnboardingTaskListBuilderServiceTest.java

--- OnboardingTaskListBuilderServiceTest.java
+++ OnboardingTaskListBuilderServiceTest.java
@@ -1,1 +1,22 @@
-readonly deletedEntries = signal<readonly string[]>([]);
+if (client.type === 'PHYSICAL_PERSON') {
+  this.applyPhysicalPersonChanges(
+    client as IPhysicalPerson,
+    changes
+  );
+
+  this.applyPhysicalPersonDeletes(
+    client as IPhysicalPerson,
+    deletedEntries
+  );
+}
+
+private applyPhysicalPersonDeletes(
+  client: IPhysicalPerson,
+  deletedEntries: readonly string[]
+): void {
+  deletedEntries.forEach(id => {
+    if (id.startsWith('nationalities:manual-')) {
+      this.removeNationality(client, id);
+    }
+  });
+}
