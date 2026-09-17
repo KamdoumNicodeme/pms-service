8bcccfa640c15c31be31801dab4d29921d57ac54
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-09-17 19:01:30 +0200

    Update ProfilingSection to manage changes
    Refactor ProfilingSection to handle changes and emit events.

--- OnboardingTaskListBuilderServiceTest.java
+++ OnboardingTaskListBuilderServiceTest.java
@@ -1,40 +1,11 @@
-export class ProfilingSection {
+protected onChangesChange(event: ComparisonChanges): void {
+  console.log('1 - HOLDER RECEIVED CHANGES');
+  console.log('changes =', event.changes);
+  console.log('deletedEntries =', event.deletedEntries);
 
-  private readonly data: ComparisonDataService =
-    inject(COMPARISON_DATA_SERVICE);
-
-  readonly policyNumber: InputSignal<string> =
-    input.required<string>();
-
-  readonly holder: InputSignal<IThirdParty> =
-    input.required<IThirdParty>();
-
-  readonly sectionId: InputSignal<string> =
-    input.required<string>();
-
-  readonly filter: InputSignal<ComparisonFilter> =
-    input<ComparisonFilter>('all');
-
-  readonly countersChange: OutputEmitterRef<ComparisonCounters> =
-    output<ComparisonCounters>();
-
-  readonly changesChange: OutputEmitterRef<ComparisonChanges> =
-    output<ComparisonChanges>();
-
-  protected readonly section = rxResource({
-    params: () => ({
-      policyNumber: this.policyNumber(),
-      holder: this.holder(),
-      sectionId: this.sectionId()
-    }),
-
-    stream: ({ params }) =>
-      this.data.getSection(
-        {
-          policyNumber: params.policyNumber,
-          holder: params.holder
-        },
-        params.sectionId
-      )
+  this.changesChange.emit({
+    thirdPartyId: this.holder().thirdPartyId,
+    changes: event.changes,
+    deletedEntries: event.deletedEntries
   });
 }
