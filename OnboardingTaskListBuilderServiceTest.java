3dff657c2f1374e2cf4ce6453d08027140a2d4b4
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-09-17 15:17:05 +0200

    Update OnboardingTaskListBuilderServiceTest.java

--- OnboardingTaskListBuilderServiceTest.java
+++ OnboardingTaskListBuilderServiceTest.java
@@ -1,27 +1,89 @@
-applyHolderChanges(
-  changeClientInformation: IChangeClientInformation,
-  thirdPartyId: string,
+private applyPhysicalPersonChanges(
+  client: IPhysicalPerson,
   changes: ReadonlyMap<string, Resolution>
-): IChangeClientInformation {
+): void {
 
-  const result: IChangeClientInformation =
-    structuredClone(changeClientInformation);
+  changes.forEach((resolution: Resolution, id: string): void => {
 
-  const client: IThirdParty | undefined = result.policy.clients.find(
-    (item: IThirdParty): boolean =>
-      item.thirdPartyId === thirdPartyId
-  );
+    const value = resolution.value;
 
-  if (!client) {
-    return result;
-  }
+    switch (id) {
 
-  if (client.type === 'PHYSICAL_PERSON') {
-    this.applyPhysicalPersonChanges(
-      client as IPhysicalPerson,
-      changes
-    );
-  }
+      case 'lastname':
+        client.lastname = value ?? '';
+        break;
 
-  return result;
+      case 'firstname':
+        client.firstName = value ?? '';
+        break;
+
+      case 'birth-date':
+        client.birthDate = value ?? '';
+        break;
+
+      case 'birth-country':
+        client.birthCountry = value ?? '';
+        break;
+
+      case 'status':
+        client.civilStatus = {
+          ...client.civilStatus,
+          status: value ?? ''
+        };
+        break;
+
+      case 'trustee-if-moral-person':
+        client.trusteeIfMoralPerson = value ?? '';
+        break;
+
+      case 'profession':
+        client.professionalDetails = {
+          ...client.professionalDetails,
+          profession: value ?? ''
+        };
+        break;
+
+      case 'profession-status':
+        client.professionalDetails = {
+          ...client.professionalDetails,
+          status: value ?? ''
+        };
+        break;
+
+      case 'employer-name':
+        client.professionalDetails = {
+          ...client.professionalDetails,
+          companyName: value ?? ''
+        };
+        break;
+
+      case 'industry-sector':
+        client.professionalDetails = {
+          ...client.professionalDetails,
+          sector: value ?? ''
+        };
+        break;
+
+      case 'type':
+        client.idDocument = {
+          ...client.idDocument,
+          type: value ?? ''
+        };
+        break;
+
+      case 'number':
+        client.idDocument = {
+          ...client.idDocument,
+          number: value ?? ''
+        };
+        break;
+
+      case 'expirationDate':
+        client.idDocument = {
+          ...client.idDocument,
+          expirationDate: value ?? ''
+        };
+        break;
+    }
+  });
 }
