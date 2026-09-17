391e8caea4290cda938806bc3d128bfe15f60497
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-09-17 15:27:18 +0200

    Refactor nationality handling in client updates

--- OnboardingTaskListBuilderServiceTest.java
+++ OnboardingTaskListBuilderServiceTest.java
@@ -1,89 +1,34 @@
-private applyPhysicalPersonChanges(
+case 'nationalities':
+  this.applyNationalitiesChange(client, resolution);
+  break;
+
+private applyNationalitiesChange(
   client: IPhysicalPerson,
-  changes: ReadonlyMap<string, Resolution>
+  resolution: Resolution
 ): void {
 
-  changes.forEach((resolution: Resolution, id: string): void => {
+  const values: string[] = Array.isArray(resolution.value)
+    ? resolution.value
+    : resolution.value
+      ? [resolution.value]
+      : [];
 
-    const value = resolution.value;
+  client.nationality = {
+    ...client.nationality,
 
-    switch (id) {
+    first: {
+      ...client.nationality?.first,
+      country: values[0] ?? null,
+    },
 
-      case 'lastname':
-        client.lastname = value ?? '';
-        break;
+    second: {
+      ...client.nationality?.second,
+      country: values[1] ?? null,
+    },
 
-      case 'firstname':
-        client.firstName = value ?? '';
-        break;
-
-      case 'birth-date':
-        client.birthDate = value ?? '';
-        break;
-
-      case 'birth-country':
-        client.birthCountry = value ?? '';
-        break;
-
-      case 'status':
-        client.civilStatus = {
-          ...client.civilStatus,
-          status: value ?? ''
-        };
-        break;
-
-      case 'trustee-if-moral-person':
-        client.trusteeIfMoralPerson = value ?? '';
-        break;
-
-      case 'profession':
-        client.professionalDetails = {
-          ...client.professionalDetails,
-          profession: value ?? ''
-        };
-        break;
-
-      case 'profession-status':
-        client.professionalDetails = {
-          ...client.professionalDetails,
-          status: value ?? ''
-        };
-        break;
-
-      case 'employer-name':
-        client.professionalDetails = {
-          ...client.professionalDetails,
-          companyName: value ?? ''
-        };
-        break;
-
-      case 'industry-sector':
-        client.professionalDetails = {
-          ...client.professionalDetails,
-          sector: value ?? ''
-        };
-        break;
-
-      case 'type':
-        client.idDocument = {
-          ...client.idDocument,
-          type: value ?? ''
-        };
-        break;
-
-      case 'number':
-        client.idDocument = {
-          ...client.idDocument,
-          number: value ?? ''
-        };
-        break;
-
-      case 'expirationDate':
-        client.idDocument = {
-          ...client.idDocument,
-          expirationDate: value ?? ''
-        };
-        break;
-    }
-  });
+    third: {
+      ...client.nationality?.third,
+      country: values[2] ?? null,
+    },
+  };
 }
