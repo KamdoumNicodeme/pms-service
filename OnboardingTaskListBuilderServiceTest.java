8c985728feb9dc067497c885d7282f2f33e77635
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-09-17 18:17:11 +0200

    Refactor applyHolderChanges to onChangesChange

--- OnboardingTaskListBuilderServiceTest.java
+++ OnboardingTaskListBuilderServiceTest.java
@@ -1,54 +1,22 @@
-applyHolderChanges(
-  changeClientInformation: IChangeClientInformation,
-  thirdPartyId: string,
-  changes: ReadonlyMap<string, Resolution>,
-  deletedEntries: readonly string[]
-): IChangeClientInformation {
+protected onChangesChange(
+  changes: ReadonlyMap<string, Resolution>
+): void {
 
-  const result: IChangeClientInformation =
-    structuredClone(changeClientInformation);
+  console.log('1 - HOLDER RECEIVED CHANGES');
 
-  const client: IThirdParty | undefined =
-    result.policy.clients.find(
-      (item: IThirdParty): boolean =>
-        item.thirdPartyId === thirdPartyId
-    );
+  changes.forEach((resolution, id) => {
+    console.log('   id =', id);
+    console.log('   resolution =', resolution);
+  });
 
-  if (!client) {
-    return result;
-  }
+  console.log(
+    '2 - deletedEntries =',
+    this.deletedEntries()
+  );
 
-  if (client.type === 'PHYSICAL_PERSON') {
-    const physicalPerson = client as IPhysicalPerson;
-
-    // Modifications + ajouts
-    this.applyPhysicalPersonChanges(
-      physicalPerson,
-      changes
-    );
-
-    // Suppressions
-    deletedEntries.forEach((id: string) => {
-      if (id.startsWith('nationalities:manual-')) {
-        this.removeNationalityFromClient(
-          physicalPerson,
-          id
-        );
-      }
-    });
-
-    return result;
-  }
-
-  // MORAL PERSON
-  if (client.type === 'MORAL_PERSON') {
-    this.applyMoralPersonChanges(
-      client as IMoralPerson,
-      changes
-    );
-
-    return result;
-  }
-
-  return result;
+  this.changesChange.emit({
+    thirdPartyId: this.holder().thirdPartyId,
+    changes,
+    deletedEntries: this.deletedEntries()
+  });
 }
