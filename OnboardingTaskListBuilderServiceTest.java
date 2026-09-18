5b485cdb8caa58de637e6b158cc92cdd92d9fdf1
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-09-18 09:10:05 +0200

    Refactor client property updates in test file

--- OnboardingTaskListBuilderServiceTest.java
+++ OnboardingTaskListBuilderServiceTest.java
@@ -1,8 +1,99 @@
-case 'name':
-    console.log('[MORAL NAME] BEFORE =', client.name);
-    console.log('[MORAL NAME] VALUE =', value);
+changes.forEach((resolution: Resolution, id: string): void => {
 
-    client.name = value ?? '';
+    console.log('[MORAL 1] ENTER id =', id);
 
-    console.log('[MORAL NAME] AFTER =', client.name);
-    break;
+    const value: string | null = resolution.value;
+
+    if (id.startsWith('tax-information:manual-')) {
+
+        console.log('[MORAL RETURN] manual tax');
+
+        return;
+
+    }
+
+    if (id.startsWith('emails:manual-')) {
+
+        console.log('[MORAL RETURN] manual email');
+
+        this.applyManualEmailChange(client, resolution);
+
+        return;
+
+    }
+
+    if (id.startsWith('emails:')) {
+
+        console.log('[MORAL RETURN] email');
+
+        this.applyEmailChange(client, id, resolution);
+
+        return;
+
+    }
+
+    if (id.startsWith('phone-numbers:manual-')) {
+
+        console.log('[MORAL RETURN] manual phone');
+
+        this.applyManualPhoneChange(client, resolution);
+
+        return;
+
+    }
+
+    if (id.startsWith('phone-numbers:')) {
+
+        console.log('[MORAL RETURN] phone');
+
+        this.applyPhoneChange(client, id, resolution);
+
+        return;
+
+    }
+
+    if (id.startsWith('tax-information:')) {
+
+        console.log('[MORAL RETURN] tax');
+
+        this.applyTaxInformationChange(client, id, resolution);
+
+        return;
+
+    }
+
+    console.log('[MORAL 2] BEFORE SWITCH id =', id);
+
+    switch (id) {
+
+        case 'thirdPartyId':
+
+            break;
+
+        case 'name':
+
+            console.log('[MORAL 3] NAME CASE value =', value);
+
+            client.name = value ?? '';
+
+            console.log('[MORAL 4] NEW NAME =', client.name);
+
+            break;
+
+        case 'industry-sector':
+
+            client.economicSector = this.stringValue(value);
+
+            break;
+
+        case 'creation-date':
+
+            client.creationDate = this.stringValue(value);
+
+            break;
+
+        case 'vat-number':
+
+            client.vatNumber = this.stringValue(value);
+
+            break;
