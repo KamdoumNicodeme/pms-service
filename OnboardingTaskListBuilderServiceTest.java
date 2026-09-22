f65c001928c39381537f11350543d40e51eeadb6
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-09-22 14:54:51 +0200

    Refactor ClientProfilingCaseRestApiModelMapper methods

--- OnboardingTaskListBuilderServiceTest.java
+++ OnboardingTaskListBuilderServiceTest.java
@@ -1,99 +1,14 @@
-@Mapper(uses = DateMapper.class)
-public interface ClientProfilingCaseRestApiModelMapper {
+public static ClientProfilingThirdPartyDto buildThirdPartyDto() {
 
-    ClientProfilingCaseDataDto toDto(
-            ClientProfilingCaseData clientProfilingCaseData
-    );
-
-    ChangeClientInformationDto toDto(
-            ChangeClientInformation changeClientInformation
-    );
-
-    ChangeClientInformation toDomain(
-            ChangeClientInformationDto changeClientInformationDto
-    );
-
-
-    // =========================================================
-    // DOMAIN -> DTO
-    // =========================================================
-
-    default ClientProfilingThirdPartyDto toThirdPartyDto(
-            ClientProfilingThirdParty clientProfilingThirdParty
-    ) {
-
-        if (clientProfilingThirdParty == null) {
-            return null;
-        }
-
-        return switch (clientProfilingThirdParty.getType()) {
-
-            case MORAL_PERSON ->
-                    toMoralPersonDto(clientProfilingThirdParty);
-
-            case PHYSICAL_PERSON ->
-                    toPhysicalPersonDto(clientProfilingThirdParty);
-
-            default ->
-                    throw new IllegalArgumentException(
-                            String.format(
-                                    "Third party type '%s' not supported",
-                                    clientProfilingThirdParty.getType()
-                            )
-                    );
-        };
-    }
-
-    ClientProfilingThirdPartyDto.PhysicalPersonDtoClientProfiling toPhysicalPersonDto(
-            ClientProfilingThirdParty clientProfilingThirdParty
-    );
-
-    ClientProfilingThirdPartyDto.MoralPersonDtoClientProfiling toMoralPersonDto(
-            ClientProfilingThirdParty clientProfilingThirdParty
-    );
-
-
-    // =========================================================
-    // DTO -> DOMAIN
-    // =========================================================
-
-    default ClientProfilingThirdParty toThirdParty(
-            ClientProfilingThirdPartyDto clientProfilingThirdPartyDto
-    ) {
-
-        if (clientProfilingThirdPartyDto == null) {
-            return null;
-        }
-
-        return switch (clientProfilingThirdPartyDto.getType()) {
-
-            case PHYSICAL_PERSON ->
-                    toPhysicalPerson(
-                            (ClientProfilingThirdPartyDto.PhysicalPersonDtoClientProfiling)
-                                    clientProfilingThirdPartyDto
-                    );
-
-            case MORAL_PERSON ->
-                    toMoralPerson(
-                            (ClientProfilingThirdPartyDto.MoralPersonDtoClientProfiling)
-                                    clientProfilingThirdPartyDto
-                    );
-
-            default ->
-                    throw new IllegalArgumentException(
-                            String.format(
-                                    "Third party type '%s' not supported",
-                                    clientProfilingThirdPartyDto.getType()
-                            )
-                    );
-        };
-    }
-
-    ClientProfilingThirdParty toPhysicalPerson(
-            ClientProfilingThirdPartyDto.PhysicalPersonDtoClientProfiling clientProfilingThirdPartyDto
-    );
-
-    ClientProfilingThirdParty toMoralPerson(
-            ClientProfilingThirdPartyDto.MoralPersonDtoClientProfiling clientProfilingThirdPartyDto
-    );
+    return ClientProfilingThirdPartyDto.PhysicalPersonDtoClientProfiling
+        .builder()
+        .type("PHYSICAL_PERSON")
+        .thirdPartyId("0003310939")
+        .aeoiStatus(TEST_AEOI_STATUS)
+        .fatcaStatus(TEST_FATCA_STATUS)
+        .taxCountry(TEST_COUNTRY)
+        .taxNumber("8798546")
+        .legalAddress(buildLegalAddressDto())
+        .taxInformations(List.of(buildTaxInformationDto()))
+        .build();
 }
