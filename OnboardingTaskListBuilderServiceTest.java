0aac77c7299839f203d5eb3128546e941d92aca7
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-09-24 10:06:40 +0200

    Update OnboardingTaskListBuilderServiceTest.java

--- OnboardingTaskListBuilderServiceTest.java
+++ OnboardingTaskListBuilderServiceTest.java
@@ -1,22 +1,11 @@
-case 'identity-documents':
-  if (this.isPhysicalPerson(digitalHolder)) {
-    return of(
-      this.coreUtils.idDocument(
-        digitalHolder,
-        kycHolder && this.isPhysicalPerson(kycHolder)
-          ? kycHolder
-          : null,
-        coreHolder && this.isPhysicalPerson(coreHolder)
-          ? coreHolder
-          : null,
-        this.idTypeOptions()
-      )
-    );
-  }
-
-  return of(
-    this.coreUtils.emptySection({
-      id: 'identity-documents',
-      title: 'Identity Documents'
-    })
-  );
+createEmpty(
+  data: IClientProfilingData
+): IChangeClientInformation {
+  return {
+    policy: {
+      policyNumber:
+        data.initialBusinessData.policy.policyNumber,
+      clients: []
+    }
+  } as IChangeClientInformation;
+}
