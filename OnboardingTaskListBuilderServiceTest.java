728e097b1c7252f71bcac17e2531933b5d7b6f8b
Jean Nicodeme <36440463+KamdoumNicodeme@users.noreply.github.com>
2026-09-25 15:58:27 +0200

    Refactor task details inputs computation logic

--- OnboardingTaskListBuilderServiceTest.java
+++ OnboardingTaskListBuilderServiceTest.java
@@ -1,62 +1,30 @@
-protected readonly fatcaOptions = computed<ComparisonOption[]>(() =>
-  (this.references()['FATCA_STATUS'] ?? []).map(ref => ({
-    value: ref.value,
-    label: ref.key,
-  }))
-);
+readonly taskDetailsInputs = computed(() => {
+  const currentCase = this.sourceCase();
+  const task = this.task();
 
-protected readonly crsOptions = computed<ComparisonOption[]>(() =>
-  (this.references()['AEOI_STATUS'] ?? []).map(ref => ({
-    value: ref.value,
-    label: ref.key,
-  }))
-);
+  if (!currentCase || !task) {
+    return {};
+  }
 
-protected readonly countryOptions = computed<ComparisonOption[]>(() =>
-  (this.references()['COUNTRY'] ?? []).map(ref => ({
-    value: ref.value,
-    label: ref.key,
-  }))
-);
+  const commonInputs = {
+    currentTask: task,
+    currentCase,
+    policy: this.policies(),
+    positions: this.positions().flat(),
+  };
 
-protected readonly professionOptions = computed<ComparisonOption[]>(() =>
-  (this.references()['PROFESSION'] ?? []).map(ref => ({
-    value: ref.value,
-    label: ref.key,
-  }))
-);
+  switch (currentCase.caseType) {
+    case CaseType.CLIENT_PROFILING:
+      return {
+        ...commonInputs,
+        caseDocuments: this.caseDocuments(),
+        clientProfilingReferences: this.clientProfilingReferences(),
+      };
 
-protected readonly industrySectorOptions = computed<ComparisonOption[]>(() =>
-  (this.references()['INDUSTRY_SECTOR'] ?? []).map(ref => ({
-    value: ref.value,
-    label: ref.key,
-  }))
-);
+    case CaseType.CHANGE_OF_STRATEGY:
+      return commonInputs;
 
-protected readonly statusMaritalOptions = computed<ComparisonOption[]>(() =>
-  (this.references()['STATUS_MARITAL'] ?? []).map(ref => ({
-    value: ref.value,
-    label: ref.key,
-  }))
-);
-
-protected readonly professionalStatusOptions = computed<ComparisonOption[]>(() =>
-  (this.references()['PROFESSIONAL_STATUS'] ?? []).map(ref => ({
-    value: ref.value,
-    label: ref.key,
-  }))
-);
-
-protected readonly tinReasonOptions = computed<ComparisonOption[]>(() =>
-  (this.references()['TIN_REASON'] ?? []).map(ref => ({
-    value: ref.value,
-    label: ref.key,
-  }))
-);
-
-protected readonly idTypeOptions = computed<ComparisonOption[]>(() =>
-  (this.references()['ID_TYPE'] ?? []).map(ref => ({
-    value: ref.value,
-    label: ref.key,
-  }))
-);
+    default:
+      return {};
+  }
+});
